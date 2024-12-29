74f4a3102a9ed228ba7fa9ec93a2d327d0c34a43
veer0p <atodariyaveer1331@gmail.com>
2024-12-29 15:37:17 +0530

    Initial Commit

---
+++ src/data/contacts.js
@@ -0,0 +1,82 @@
+import veer from "assets/images/image.png";
+
+const users = [
+  {
+    id: 1,
+    profile_picture: veer,
+    name: "Bn 👻",
+    phone_number: "+91 8487005334",
+    whatsapp_name: null,
+    unread: 0,
+    messages: {
+      "5/23/22": [
+        {
+          content: "media not available",
+          sender: null,
+          time: "01:58",
+          status: "read",
+        },
+      ],
+      //     },
+      //     {
+      //       content: "🥰👍",
+      //       sender: "Veer",
+      //       time: "01:58",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "Malhar thaakar jevo dekhai chhe",
+      //       sender: null,
+      //       time: "01:58",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "Ha",
+      //       sender: "Veer",
+      //       time: "01:58",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "Mast lage 6 baki",
+      //       sender: "Veer",
+      //       time: "01:59",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "Ye sari bate insta pe hi rakho",
+      //       sender: "Veer",
+      //       time: "01:59",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "😅",
+      //       sender: "Veer",
+      //       time: "01:59",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "Ha wahi",
+      //       sender: null,
+      //       time: "02:00",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "Yaha pe sare achhe kam",
+      //       sender: "Veer",
+      //       time: "02:01",
+      //       status: "read",
+      //     },
+      //     {
+      //       content: "Yes yes 🤣",
+      //       sender: null,
+      //       time: "02:01",
+      //       status: "read",
+      //     },
+      //   ],
+    },
+    group: false,
+    pinned: false,
+    typing: false,
+  },
+];
+export default users;
---
+++ src/data/contacts1.js
@@ -0,0 +1,46 @@
+import ppGirl2 from "assets/images/profile-picture-girl-2.jpeg";
+import ppGirl3 from "assets/images/profile-picture-girl-3.jpeg";
+import ppGirl4 from "assets/images/profile-picture-girl-4.jpeg";
+import ppBoy2 from "assets/images/profile-picture-boy-2.jpeg";
+import ppBoy3 from "assets/images/profile-picture-boy-3.jpeg";
+import getRandomSentence from "utils/getRandomSentence";
+
+const users = [
+  {
+    id: 1,
+    profile_picture: "img/url.jpeg",
+    name: "Bn 👻",
+    phone_number: "+91 8487005334",
+    whatsapp_name: "Bn 👻",
+    unread: 0,
+    messages: {
+      // "23/05/2022": [
+      //   {
+      //     content: "media not available",
+      //     sender: "Moon",
+      //     time: "01:58",
+      //     status: "read",
+      //   },
+      //   {
+      //     content: "🥰👍",
+      //     sender: "Veer",
+      //     time: "01:58",
+      //     status: "read",
+      //   },
+      //   {
+      //     content: "Malhar thaakar jevo dekhai chhe",
+      //     sender: "Moon",
+      //     time: "09:11:26",
+      //     status: "read",
+      //   },
+      // ],
+
+      "24/05/2022": [],
+    },
+    group: false,
+    pinned: true,
+    typing: false,
+  },
+];
+
+export default users;
